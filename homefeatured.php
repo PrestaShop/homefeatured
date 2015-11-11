4c29188e92c519572ecbf73546c48c69bea08b48
SebSept <sebastienmonterisi@yahoo.fr>
2015-11-11 22:40:21 +0100

    renamed $nbr to $number_of_products_to_display
    expressiveness

--- homefeatured.php
+++ homefeatured.php
@@ -86,9 +86,9 @@ class HomeFeatured extends Module
 		$output = '';
 		$errors = array();
 		if (Tools::isSubmit('submitHomeFeatured')) {
-			$nbr = Tools::getValue('HOME_FEATURED_NBR');
-			if (!Validate::isInt($nbr) || $nbr <= 0)
-			$errors[] = $this->l('The number of products is invalid. Please enter a positive number.');
+			$number_of_products_to_display = Tools::getValue('HOME_FEATURED_NBR');
+			if (!Validate::isInt($number_of_products_to_display) || $number_of_products_to_display <= 0)
+				$errors[] = $this->l('The number of products is invalid. Please enter a positive number.');
 
 			$cat = Tools::getValue('HOME_FEATURED_CAT');
 			if (!Validate::isInt($cat) || $cat <= 0) {
@@ -103,8 +103,8 @@ class HomeFeatured extends Module
 				$output = $this->displayError(implode('<br />', $errors));
             }
 			else {
-				Configuration::updateValue('HOME_FEATURED_NBR', (int)$nbr);
 				Configuration::updateValue('HOME_FEATURED_CAT', (int)$cat);
+				Configuration::updateValue('HOME_FEATURED_NBR', (int)$number_of_products_to_display);
 				Configuration::updateValue('HOME_FEATURED_RANDOMIZE', (bool)$rand);
 				Tools::clearCache(Context::getContext()->smarty, $this->getTemplatePath('homefeatured.tpl'));
 				$output = $this->displayConfirmation($this->l('Your settings have been updated.'));
